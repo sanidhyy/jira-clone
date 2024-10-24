d6d3b8942a26e9c8bec2e2372e5544fee86fbc3f
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-10-24 16:10:45 +0530

    added customized shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,47 @@
+import Link from 'next/link';
+
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
 
 const HomePage = () => {
   return (
-    <main>
-      <h1 className="text-3xl text-emerald-500">Hello, World!</h1>
+    <main className="space-x-2 space-y-2">
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="tertiary">Tertiary</Button>
+      <Button disabled={true}>Disabled</Button>
 
-      <Button size="lg" variant="destructive">
-        Click me
-      </Button>
+      <hr />
+
+      <Button>Default</Button>
+      <Button size="icon">😊</Button>
+      <Button size="lg">Large</Button>
+      <Button size="sm">Small</Button>
+      <Button size="xs">Extra Small</Button>
+
+      <hr />
+
+      <Input placeholder="Type something here..." className="w-[400px]" />
+
+      <hr />
+
+      <Select>
+        <SelectTrigger className="w-[400px]">
+          <SelectValue placeholder="Choose a fruit" />
+        </SelectTrigger>
+
+        <SelectContent>
+          <SelectItem value="apple">Apple</SelectItem>
+          <SelectItem value="banana">Banana</SelectItem>
+          <SelectItem value="mango">Mango</SelectItem>
+          <SelectItem value="orange">Orange</SelectItem>
+        </SelectContent>
+      </Select>
     </main>
   );
 };
