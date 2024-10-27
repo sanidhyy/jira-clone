76058bc3131d1baccd2bd3d915ce1a2423c73854
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-10-27 12:43:35 +0530

    handle login, register and logout routes with appwrite

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,22 +1,53 @@
 import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
+import { deleteCookie, setCookie } from 'hono/cookie';
+import { ID } from 'node-appwrite';
 
+import { AUTH_COOKIE } from '@/features/auth/constants';
 import { signInFormSchema, signUpFormSchema } from '@/features/auth/schema';
+import { createAdminClient } from '@/lib/appwrite';
 
 const app = new Hono()
   .post('/login', zValidator('json', signInFormSchema), async (ctx) => {
     const { email, password } = ctx.req.valid('json');
 
-    console.log({ email, password });
+    const { account } = await createAdminClient();
 
-    return ctx.json({ email, password });
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(ctx, AUTH_COOKIE, session.secret, {
+      path: '/',
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return ctx.json({ success: true });
   })
   .post('/register', zValidator('json', signUpFormSchema), async (ctx) => {
     const { name, email, password } = ctx.req.valid('json');
 
-    console.log({ name, email, password });
+    const { account } = await createAdminClient();
 
-    return ctx.json({ name, email, password });
+    await account.create(ID.unique(), email, password, name);
+
+    const session = await account.createEmailPasswordSession(email, password);
+
+    setCookie(ctx, AUTH_COOKIE, session.secret, {
+      path: '/',
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      maxAge: 60 * 60 * 24 * 30,
+    });
+
+    return ctx.json({ success: true });
+  })
+  .post('/logout', (ctx) => {
+    deleteCookie(ctx, AUTH_COOKIE);
+
+    return ctx.json({ success: true });
   });
 
 export default app;
