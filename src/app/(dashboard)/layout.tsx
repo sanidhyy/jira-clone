f39c0a30d9ffed1c27967fd3005c643bf02d5168
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-10-27 18:53:59 +0530

    basic sidebar implemented

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+import type { PropsWithChildren } from 'react';
+
+import { Sidebar } from '@/components/sidebar';
+
+const DashboardLayout = ({ children }: PropsWithChildren) => {
+  return (
+    <div className="min-h-screen">
+      <div className="flex size-full">
+        <div className="fixed left-0 top-0 hidden lg:block lg:w-[264px] h-full overflow-auto">
+          <Sidebar />
+        </div>
+
+        <div className="lg:pl-[264px]">
+          <div className="mx-auto max-w-screen-xl h-full">
+            {/* TODO: Navbar */}
+
+            <main className="h-full py-8 px-6 flex flex-col">{children}</main>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default DashboardLayout;
