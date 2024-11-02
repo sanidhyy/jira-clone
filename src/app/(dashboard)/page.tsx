50c1e18fe68da4ee4fd9c2079448928c3bface78
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-11-02 16:56:03 +0530

    changed from actions to queries for better readability

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 
-import { getCurrent } from '@/features/auth/actions';
-import { getWorkspaces } from '@/features/workspaces/actions';
+import { getCurrent } from '@/features/auth/queries';
+import { getWorkspaces } from '@/features/workspaces/queries';
 
 const HomePage = async () => {
   const user = await getCurrent();
