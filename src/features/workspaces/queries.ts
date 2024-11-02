d1e3e204f47bd9481ac8f56f38d54f83b7ed1905
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-11-02 17:04:22 +0530

    refactor appwrite create session client

--- src/features/workspaces/queries.ts
+++ src/features/workspaces/queries.ts
@@ -1,29 +1,16 @@
 'use server';
 
-import { cookies } from 'next/headers';
-import { Account, Client, Databases, type Models, Query, Storage } from 'node-appwrite';
+import { type Models, Query } from 'node-appwrite';
 
 import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from '@/config/db';
-import { AUTH_COOKIE } from '@/features/auth/constants';
 import { getMember } from '@/features/members/utils';
+import { createSessionClient } from '@/lib/appwrite';
 
 import { Workspace } from './types';
 
 export const getWorkspaces = async () => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-    const session = cookies().get(AUTH_COOKIE);
-
-    if (!session) return { documents: [], total: 0 };
-
-    client.setSession(session.value);
-
-    const account = new Account(client);
-    const databases = new Databases(client);
-    const storage = new Storage(client);
+    const { account, databases, storage } = await createSessionClient();
 
     const user = await account.get();
     const members = await databases.listDocuments(DATABASE_ID, MEMBERS_ID, [Query.equal('userId', user.$id)]);
@@ -68,19 +55,7 @@ interface GetWorkspaceProps {
 
 export const getWorkspace = async ({ workspaceId }: GetWorkspaceProps) => {
   try {
-    const client = new Client()
-      .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!)
-      .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
-
-    const session = cookies().get(AUTH_COOKIE);
-
-    if (!session) return null;
-
-    client.setSession(session.value);
-
-    const account = new Account(client);
-    const databases = new Databases(client);
-    const storage = new Storage(client);
+    const { account, databases, storage } = await createSessionClient();
 
     const user = await account.get();
     const member = await getMember({
--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,5 +1,33 @@
+import { cookies } from 'next/headers';
 import { Account, Client, Databases, Storage, Users } from 'node-appwrite';
 import 'server-only';
+
+import { AUTH_COOKIE } from '@/features/auth/constants';
+
+export async function createSessionClient() {
+  const client = new Client().setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!).setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+
+  const session = cookies().get(AUTH_COOKIE);
+
+  if (!session || !session.value) throw new Error('Unauthorized.');
+
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get databases() {
+      return new Databases(client);
+    },
+    get storage() {
+      return new Storage(client);
+    },
+    get users() {
+      return new Users(client);
+    },
+  };
+}
 
 export async function createAdminClient() {
   const client = new Client()
