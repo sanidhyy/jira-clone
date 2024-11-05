287734200b86fcc8d64d33565d41ed7f5f8db955
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-11-05 19:14:30 +0530

    create project api implemented

---
+++ src/features/projects/schema.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+export const createProjectSchema = z.object({
+  name: z.string().trim().min(1, 'Project name is required.'),
+  image: z.union([z.instanceof(File), z.string().transform((value) => (value === '' ? undefined : value))]).optional(),
+  workspaceId: z.string({
+    message: 'Workspace id is required.',
+  }),
+});
--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -1,26 +1,20 @@
 import { zValidator } from '@hono/zod-validator';
 import { Hono } from 'hono';
-import { Query } from 'node-appwrite';
+import { ID, Query } from 'node-appwrite';
 import { z } from 'zod';
 
-import { DATABASE_ID, PROJECTS_ID } from '@/config/db';
+import { DATABASE_ID, IMAGES_BUCKET_ID, PROJECTS_ID } from '@/config/db';
 import { getMember } from '@/features/members/utils';
+import { createProjectSchema } from '@/features/projects/schema';
 import { sessionMiddleware } from '@/lib/session-middleware';
 
-const app = new Hono().get(
-  '/',
-  sessionMiddleware,
-  zValidator(
-    'query',
-    z.object({
-      workspaceId: z.string(),
-    }),
-  ),
-  async (ctx) => {
-    const user = ctx.get('user');
+const app = new Hono()
+  .post('/', sessionMiddleware, zValidator('form', createProjectSchema), async (ctx) => {
     const databases = ctx.get('databases');
+    const storage = ctx.get('storage');
+    const user = ctx.get('user');
 
-    const { workspaceId } = ctx.req.valid('query');
+    const { name, image, workspaceId } = ctx.req.valid('form');
 
     const member = await getMember({
       databases,
@@ -32,13 +26,57 @@ const app = new Hono().get(
       return ctx.json({ error: 'Unauthorized.' }, 401);
     }
 
-    const projects = await databases.listDocuments(DATABASE_ID, PROJECTS_ID, [
-      Query.equal('workspaceId', workspaceId),
-      Query.orderDesc('$createdAt'),
-    ]);
+    let uploadedImageId: string | undefined = undefined;
 
-    return ctx.json({ data: projects });
-  },
-);
+    if (image instanceof File) {
+      const file = await storage.createFile(IMAGES_BUCKET_ID, ID.unique(), image);
+
+      uploadedImageId = file.$id;
+    } else {
+      uploadedImageId = image;
+    }
+
+    const project = await databases.createDocument(DATABASE_ID, PROJECTS_ID, ID.unique(), {
+      name,
+      userId: user.$id,
+      imageId: uploadedImageId,
+      workspaceId,
+    });
+
+    return ctx.json({ data: project });
+  })
+  .get(
+    '/',
+    sessionMiddleware,
+    zValidator(
+      'query',
+      z.object({
+        workspaceId: z.string(),
+      }),
+    ),
+    async (ctx) => {
+      const user = ctx.get('user');
+      const databases = ctx.get('databases');
+
+      const { workspaceId } = ctx.req.valid('query');
+
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
+
+      if (!member) {
+        return ctx.json({ error: 'Unauthorized.' }, 401);
+      }
+
+      const projects = await databases.listDocuments(DATABASE_ID, PROJECTS_ID, [
+        Query.equal('workspaceId', workspaceId),
+        Query.orderDesc('$createdAt'),
+      ]);
+
+      return ctx.json({ data: projects });
+    },
+  );
 
 export default app;
